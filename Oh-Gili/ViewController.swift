3aad63c14f9b2b0492b86171faa8f8564eacd3fb
tomokazutakahashi4 <takahashitomokazu4@gmail.com>
2020-07-19 16:26:45 +0900

    Initial Commit

---
+++ Oh-Gili/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oh-Gili
+//
+//  Created by Raphael on 2020/07/19.
+//  Copyright © 2020 takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
